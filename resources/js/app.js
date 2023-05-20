d1d3dc82b1d1f0df7727f6370db55fbc4a194608
Sandro Nilton <sandro91111@gmail.com>
2023-05-20 13:09:49 -0500

    Update home config procedure 5

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,8 @@ import AlpineFloatingUI from '@awcodes/alpine-floating-ui';
 import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm';
 
 Alpine.plugin(focus);
-Alpine.plugin(AlpineFloatingUI)
-Alpine.plugin(NotificationsAlpinePlugin)
-Alpine.plugin(mask)
-
-window.Alpine = Alpine;
+Alpine.plugin(mask);
+Alpine.plugin(AlpineFloatingUI);
+Alpine.plugin(NotificationsAlpinePlugin);
 
 Alpine.start();
